8029f3ec77baa59b61247cf0de131b5542f3b009
xy2020 <xy.g2020@gmail.com>
2021-05-26 20:45:55 +0800

    11111

--- componment/catpad/catpad.js
+++ componment/catpad/catpad.js
@@ -26,6 +26,23 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    navTo(e){
+      console.log(e.currentTarget.dataset)
+      console.log(e)
+      var navUrl = ''
+      if(e.currentTarget.dataset.hasrecord == '0'){
+        navUrl = '../read/read?eid='
+      }else{
+        navUrl = '../translate/translate?eid='
+      }
+      wx.navigateTo({
+        url: navUrl + e.currentTarget.dataset.infoid,
+        events: {
+        },
+        success: function(res) {
+          console.log(e.currentTarget.dataset.hasrecord)
+        }
+      })
+    }
   }
 })
