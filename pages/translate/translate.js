069b5961499acadee5ba64dd75c4576aa50b5572
unknown <unknown@example.com>
2021-05-26 20:42:33 +0800

    ffffff

--- pages/translate/translate.js
+++ pages/translate/translate.js
@@ -1,66 +1,43 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-26 16:07:26
+ * @LastEditTime: 2021-05-26 20:42:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \miaoER\pages\translate\translate.js
+ */
 // pages/translate/translate.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        collect: false,
+        ToView: ""
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    changeCollect: function() {
+        if (this.data.collect) {
+            this.setData({
+                collect: false
+            })
+        } else {
+            this.setData({
+                collect: true
+            })
+        }
+    },
+    changeposition: function() {
+        this.setData({
+            ToView: "header"
+        })
+    }
 })
