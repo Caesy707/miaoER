3b70f0e41f9aebd4cd81f44eb061fe5100085087
unknown <unknown@example.com>
2021-05-27 21:21:38 +0800

    11111

--- pages/translate/translate.js
+++ pages/translate/translate.js
@@ -121,12 +121,115 @@ Page({
 
         }
     },
+
+    changeCollect: function() {
+        var that = this
+        // if (this.data.collect) {
+        //     this.setData({
+        //         collect: false
+        //     })
+        // } else {
+        //     this.setData({
+        //         collect: true
+        //     })
+        // }
+        console.log(that.data)
+        if(this.data.collect){
+            wx.request({
+
+                url: 'https://wx.bitaxes.com/api/episode/star', 
+                method: 'POST',
+                header: {
+                  'content-type': 'application/json' 
+                },
+                data:{
+                  "uid": that.data.uid,
+                  "eid": that.data.eid,
+                },
+                success(res) {
+                    var epi = that.data.episode
+                    epi.has_star = 0
+                    that.setData({
+                        collect: false,
+                        episode: epi
+                    })
+                  console.log(res.data)
+                }
+              })
+        }else{
+
+            wx.request({
+
+                url: 'https://wx.bitaxes.com/api/episode/star', 
+                method: 'POST',
+                header: {
+                  'content-type': 'application/json' 
+                },
+                data:{
+                  "uid": that.data.uid,
+                  "eid": that.data.eid,
+                },
+                success(res) {
+                    var epi = that.data.episode
+                    epi.has_star = 1
+                    that.setData({
+                        collect: true,
+                        episode: epi
+                    })
+                  console.log(res.data)
+                }
+              })
+          
+
+        }
+    },
     changeposition: function() {
         this.setData({
             ToView: "header"
         })
     },
     clickStar:function(e){
+        var that = this 
+        console.log(e) 
+ 
+ 
+        wx.request({ 
+ 
+            url: 'https://wx.bitaxes.com/api/episode/note/like',  
+            method: 'POST', 
+            header: { 
+              'content-type': 'application/json'  
+            }, 
+            data:{ 
+              "uid": that.data.uid, 
+              "cid": e.currentTarget.dataset.cid, 
+            }, 
+            success(res) { 
+                var epi = that.data.episode 
+                epi.notes[e.currentTarget.dataset.index].like_count = res.data.data.like_count; 
+                epi.notes[e.currentTarget.dataset.index].has_like = res.data.data.has_like; 
+                that.setData({ 
+                    episode:epi 
+                }) 
+              console.log(res.data) 
+            } 
+          }) 
+    },
+    changePattern:function(e){
         console.log(e)
-    }
+        wx.setStorage({
+            key: "AnsEpisode",
+            data: this.data.episode
+        })
+        // wx.setStorage({
+        //     key: "Answer",
+        //     data: this.data.Answer
+        // })
+        wx.navigateTo({
+            url: '../Ans/Ans?eid=' + this.data.eid + '&uid=' + this.data.uid,
+        })
+    },
+    onunload: function() {
+        wx.removeStorageSync('AnsEpisode');
+    },
 })
