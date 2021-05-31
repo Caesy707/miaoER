9e452064a58a409990271ac07aaf1525cf3e1c04
xy2020 <xy.g2020@gmail.com>
2021-05-31 10:37:29 +0800

    1111111

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,6 +15,7 @@ Page({
         userInfo: {},
         hasUserInfo: false,
         canIUseGetUserProfile: false,
+        hasUserInfoInStorage: true,
         isShow: false,
         isShowtoo: false,
         uid: 0,
@@ -29,11 +30,7 @@ Page({
     },
     onLoad() {
         var that = this;
-        if (wx.getUserProfile) {
-            this.setData({
-                canIUseGetUserProfile: true
-            })
-        }
+       
         // wx.clearStorage({
         //     complete: (res) => {
         //         console.log("clear")
@@ -45,193 +42,196 @@ Page({
             success: (res) => {
                 console.log(res)
             },
-            fail: (res)=>{
+            fail: (res) => {
                 console.log(res)
-        wx.checkSession({
-            success() {
-                console.log("未过期")
-                wx.login({
-                    success(res) {
-                        if (res.code) {
-                            //发起网络请求
-                            wx.request({
-                                url: 'https://wx.bitaxes.com/api/wx/user/login/' + res.code,
-                                success(reqRes) {
-                                    console.log(reqRes.data)
-                                    // wx.setStorage({
-                                    //     data: reqRes.data,
-                                    //     key: 'code',
-                                    // })
-                                    if (reqRes.data.data.IsNewUser) {
-                                        that.setData({
-                                            isShow: true,
-                                            uid: reqRes.data.data.id,
-                                        })
-                                    }
-                                    that.setData({
-                                        uid: reqRes.data.data.id
-                                    })
-                                    wx.setStorage({
-                                        data: reqRes.data.data.id,
-                                        key: 'uid',
-                                    })
-                                    // wx.setStorage({
-                                    //     data: reqRes.data.data,
-                                    //     key: 'user',
-                                    // })
-
-                                }
-                            })
-
-                        } else {
-                            console.log('登录失败！' + res.errMsg)
-                        }
-                    }
-                })
-            },
-            fail() {
-                wx.login({
-                    success(res) {
-                        if (res.code) {
-                            //发起网络请求
-                            wx.request({
-                                url: 'https://wx.bitaxes.com/api/wx/user/login/' + res.code,
-                                success(reqRes) {
-                                    console.log(reqRes.data)
-                                    // wx.setStorage({
-                                    //     data: reqRes.data,
-                                    //     key: 'code',
-                                    // })
-                                    wx.setStorage({
-                                        data: reqRes.data.data.id,
-                                        key: 'uid',
-                                    })
-                                    if (reqRes.data.data.IsNewUser) {
-                                        that.setData({
-                                            isShow: true,
-                                        })
-                                    }
-                                    that.setData({
-                                        uid: reqRes.data.data.id
-                                    })
-                                }
-                            })
-
-                        } else {
-                            console.log('登录失败！' + res.errMsg)
-                        }
-                    }
-                })
-            }
-        })
-    }
-})
-        
-        this.getgrade()
-        },
-    getUserProfile(e) {
-        var that = this
-        console.log(that.data.uid)
-        wx.getStorage({
-            key: 'user',
-            success: (res) => {
-                that.navigateTo(e)
-                console.log(res)
-            },
-            fail: (res)=>{
-                console.log(res)
-                wx.getUserProfile({
-                    desc: '展示用户信息',
-                    success: (res) => {
-                        console.log(res.userInfo)
-                        this.setData({
-                            userInfo: res.userInfo,
-                            hasUserInfo: true,
-                            isShowtoo: false
-                        })
-                        wx.setStorage({
-                            data: res.userInfo,
-                            key: 'user',
-                        })
-                        // wx.getStorage({
-                        //     key: 'uid',
-                        //     success (uidRes) {
-                        //       console.log(uidRes.data)
-    
-                        //       wx.request({
-                        //         url: 'https://wx.bitaxes.com/api/wx/user/userinfo', 
-                        //         method: 'POST',
-                        //         header: {
-                        //         'content-type': 'application/json' // 默认值
-                        //         },
-                        //         data:{
-                        //             "uid": uid.data,
-                        //             "ans2": that.data.Answer[1],
-                        //             "ans3": that.data.Answer[2],
-                        //             "uid": that.data.episode.uid,
-                        //             "eid": that.data.eid,
-                        //             "spend_time":"3分21秒"
-    
-                        //         },
-                        //         success(res) {
-                        //             console.log(res.data)
-                        //         }
-                        //     })
-                        //     }
-                        // })
-    
-                        wx.request({
-                            url: 'https://wx.bitaxes.com/api/wx/user/userinfo',
-                            method: 'POST',
-                            header: {
-                                'content-type': 'application/json' // 默认值
-                            },
-                            data: {
-                                "uid": that.data.uid,
-                                "nickName": res.userInfo.nickName,
-                                "avatarUrl": res.userInfo.avatarUrl,
-                                "gender": res.userInfo.gender,
-                                "country": res.userInfo.country,
-                                "province": res.userInfo.province,
-                                "city": res.userInfo.city,
-                                "language": res.userInfo.language
-    
-    
-                            },
+                wx.checkSession({
+                    success() {
+                        console.log("未过期")
+                        wx.login({
                             success(res) {
-                                console.log(res.data)
+                                if (res.code) {
+                                    //发起网络请求
+                                    wx.request({
+                                        url: 'https://wx.bitaxes.com/api/wx/user/login/' + res.code,
+                                        success(reqRes) {
+                                            console.log(reqRes.data)
+                                            // wx.setStorage({
+                                            //     data: reqRes.data,
+                                            //     key: 'code',
+                                            // })
+                                            if (reqRes.data.data.IsNewUser) {
+                                                that.setData({
+                                                    isShow: true,
+                                                    uid: reqRes.data.data.id,
+                                                })
+                                            }
+                                            that.setData({
+                                                uid: reqRes.data.data.id
+                                            })
+                                            wx.setStorage({
+                                                data: reqRes.data.data.id,
+                                                key: 'uid',
+                                            })
+                                            // wx.setStorage({
+                                            //     data: reqRes.data.data,
+                                            //     key: 'user',
+                                            // })
+
+                                        }
+                                    })
+
+                                } else {
+                                    console.log('登录失败！' + res.errMsg)
+                                }
                             }
                         })
-                        // wx.navigateTo({
-                        //     url: '../user/user',
-                        // })
-                        that.navigateTo(e)     
                     },
-                    fail: () => {
-                        // console.log(1)
-                        // wx.navigateTo({
-                        //     url: '../user/user',
-                        // })
-                        // wx.showModal({
-                        //     title: '提示',
-                        //     content: '我们需要你的用户信息',
-                        //     success (res) {
-                        //       if (res.confirm) {
-                        //         console.log('用户点击确定')
-                        //       } else if (res.cancel) {
-                        //         console.log('用户点击取消')
-                        //       }
-                        //     }
-                        //   })
+                    fail() {
+                        wx.login({
+                            success(res) {
+                                if (res.code) {
+                                    //发起网络请求
+                                    wx.request({
+                                        url: 'https://wx.bitaxes.com/api/wx/user/login/' + res.code,
+                                        success(reqRes) {
+                                            console.log(reqRes.data)
+                                            // wx.setStorage({
+                                            //     data: reqRes.data,
+                                            //     key: 'code',
+                                            // })
+                                            wx.setStorage({
+                                                data: reqRes.data.data.id,
+                                                key: 'uid',
+                                            })
+                                            if (reqRes.data.data.IsNewUser) {
+                                                that.setData({
+                                                    isShow: true,
+                                                })
+                                            }
+                                            that.setData({
+                                                uid: reqRes.data.data.id
+                                            })
+                                        }
+                                    })
+
+                                } else {
+                                    console.log('登录失败！' + res.errMsg)
+                                }
+                            }
+                        })
                     }
                 })
             }
         })
-        // if (wx.getUserProfile) {
-            
-        // }else{
-           
-        // }
+
+        this.getgrade()
+    },
+    getUserProfile(e) {
+        console.log(this.data.canIUseGetUserProfile)
+        var that = this
+        console.log(that.data.uid)
+        // wx.getStorage({
+        //     key: 'user',
+        //     success: (res) => {
+        //         that.navigateTo(e)
+        //         console.log(res)
+        //     },
+        //     fail: (res) => {
+        //         console.log(res)
+        //         console.log("fail")
+        //     }
+        // })
+        console.log(that.data.canIUseGetUserProfile,  !that.data.hasUserInfoInStorage)
+        if (that.data.canIUseGetUserProfile && !that.data.hasUserInfoInStorage) {
+            wx.getUserProfile({
+                desc: '展示用户信息',
+                success: (res) => {
+                    console.log(res.userInfo)
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true,
+                        isShowtoo: false
+                    })
+                    wx.setStorage({
+                        data: res.userInfo,
+                        key: 'user',
+                    })
+                    // wx.getStorage({
+                    //     key: 'uid',
+                    //     success (uidRes) {
+                    //       console.log(uidRes.data)
+
+                    //       wx.request({
+                    //         url: 'https://wx.bitaxes.com/api/wx/user/userinfo', 
+                    //         method: 'POST',
+                    //         header: {
+                    //         'content-type': 'application/json' // 默认值
+                    //         },
+                    //         data:{
+                    //             "uid": uid.data,
+                    //             "ans2": that.data.Answer[1],
+                    //             "ans3": that.data.Answer[2],
+                    //             "uid": that.data.episode.uid,
+                    //             "eid": that.data.eid,
+                    //             "spend_time":"3分21秒"
+
+                    //         },
+                    //         success(res) {
+                    //             console.log(res.data)
+                    //         }
+                    //     })
+                    //     }
+                    // })
+
+                    wx.request({
+                        url: 'https://wx.bitaxes.com/api/wx/user/userinfo',
+                        method: 'POST',
+                        header: {
+                            'content-type': 'application/json' // 默认值
+                        },
+                        data: {
+                            "uid": that.data.uid,
+                            "nickName": res.userInfo.nickName,
+                            "avatarUrl": res.userInfo.avatarUrl,
+                            "gender": res.userInfo.gender,
+                            "country": res.userInfo.country,
+                            "province": res.userInfo.province,
+                            "city": res.userInfo.city,
+                            "language": res.userInfo.language
+
+
+                        },
+                        success(res) {
+                            console.log(res.data)
+                            that.navigateTo(e)
+                        }
+                    })
+                    // wx.navigateTo({
+                    //     url: '../user/user',
+                    // })
+                    
+                },
+                fail: () => {
+                    // console.log(1)
+                    // wx.navigateTo({
+                    //     url: '../user/user',
+                    // })
+                    // wx.showModal({
+                    //     title: '提示',
+                    //     content: '我们需要你的用户信息',
+                    //     success (res) {
+                    //       if (res.confirm) {
+                    //         console.log('用户点击确定')
+                    //       } else if (res.cancel) {
+                    //         console.log('用户点击取消')
+                    //       }
+                    //     }
+                    //   })
+                }
+            })
+        }else{
+            that.navigateTo(e)
+        }
     },
     navigateTo(e) {
         var index = e.currentTarget.dataset.index
@@ -276,6 +276,28 @@ Page({
     secondmask() {
         this.setData({
             isShowtoo: false
+        })
+    },
+    onShow: function(){
+        var that = this
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
+        wx.getStorage({
+            key: 'user',
+            success: (res) => {
+                that.setData({
+                    hasUserInfoInStorage: true
+                })
+            },
+            fail: (res) => {
+                console.log(res)
+                that.setData({
+                    hasUserInfoInStorage: false
+                })
+            }
         })
     }
 })
